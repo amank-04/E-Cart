c26be9d0fb66a4ed517e5edd714439ec4ca1fea7
Aman <mailamankumar01@gmail.com>
2025-05-12 23:14:22 +0530

    update the enviroment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api',
-  // apiUrl: 'https://e-cart-server.vercel.app/api',
+  // apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://e-cart-server.vercel.app/api',
 };
